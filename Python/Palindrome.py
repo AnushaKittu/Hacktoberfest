cf3b5a08946947419512404f0c6cae8ebdb3208d
AnushaKittu <44207778+AnushaKittu@users.noreply.github.com>
2019-10-12 19:14:28 +0530

    Update Palindrome.py

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -5,14 +5,11 @@ def reverse(s):
         return s[::-1] 
 
 def isPalindrome(s):
-    # Calling reverse function 
     rev = reverse(s) 
-    # Checking if both string are equal or not 
     if (s == rev): 
         return True
     return False
 
-# Driver code 
 s = str(input("Please enter the text: \n"))
 ans = isPalindrome(s.lower()) 
 
